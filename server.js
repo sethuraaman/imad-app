7a3a257ce1ae1fd2002113a63f755dea80def409
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-05 21:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
-  res.send(createTamplate(articles[articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
