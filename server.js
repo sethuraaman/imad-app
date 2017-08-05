c9cc576504e6c61b749f481466748586ea1803b1
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-05 21:38:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ var articles = {
 };
 
 function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
+    var title = data.title ;
+    var date = data.date ;
+    var heading = data.heading ;
+    var content = data.content ;
     
      var htmlTemplate = `
     <html>
