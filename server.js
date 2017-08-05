d500bf821fde2962e7e851a093779a10198484d7
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-05 12:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'article-one.html'));
+  res.sendFile(path.join(__dirname,imad-app/article-one.html));
 });
 
 app.get('/', function (req, res) {
