f6a50add689bddd526ad1e3dda352e3c6d8a7282
sethuraaman <sethu18rr@gmail.com>
2017-08-05 23:17:01 +0530

    made local change

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var express = require("express");
+var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 var articles = {
-    'articleOne': {
-        title: 'Article One by Sethuraaman',
-        heading: 'ARTICLE ONE',
-        date: '05 AUG 2017;1700',
+    "articleOne": {
+        title: "Article One by Sethuraaman",
+        heading: "ARTICLE ONE",
+        date: "05 AUG 2017;1700",
         content: `
         <p>
             this is my first web which i was tring it my own
         </p>`
     },
-    'articleTwo': {
-        title: 'Article Two by Sethuraaman',
-        heading: 'ARTICLE TWO',
-        date: '05 AUG 2017;1800',
+    "articleTwo": {
+        title: "Article Two by Sethuraaman",
+        heading: "ARTICLE TWO",
+        date: "05 AUG 2017;1800",
         content: `
             <p
                 Iam Trying my Best to Do It
             </p>`
     },
-    'articleThree': {
-        title: 'Article Three by Sethuraaman',
-        heading: 'ARTICLE THREE',
-        date: '05 AUG 2017;1900',
+    "articleThree": {
+        title: "Article Three by Sethuraaman",
+        heading: "ARTICLE THREE",
+        date: "05 AUG 2017;1900",
         content: `
         <p>
             It Might Be The Correct One
         </p>`
     },
     
-};
+}
 
 function createTemplate (data) {
     var title = data.title;
@@ -50,7 +50,7 @@ function createTemplate (data) {
                 ${title}
                 </h1>
             </title>
-            <meta name='veiwport' content= 'width=device-width' intial-scale=1>
+            <meta name="veiwport" content= "width=device-width" intial-scale=1>
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
@@ -77,25 +77,25 @@ function createTemplate (data) {
     return htmlTemplate;  
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "ui", "index.html"));
 });
 
-app.get('/:postName', function(req,res) {
+app.get("/:postName", function(req,res) {
     var postName = req.params.postName;
     res.send(createTemplate(articles[postName]));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get("/ui/style.css", function (req, res) {
+  res.sendFile(path.join(__dirname, "ui", "style.css"));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get("/ui/madi.png", function (req, res) {
+  res.sendFile(path.join(__dirname, "ui", "madi.png"));
 });
 
 
-// Do not change port, otherwise your app won't run on IMAD servers
+// Do not change port, otherwise your app won"t run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
