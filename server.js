fc9dc0d92549f5f57a17256f3a75051a1eac0195
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-06 22:48:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,10 +57,11 @@ var date = data.date;
 var heading = data.heading;
 var content = data.content;
 var htmlTemplate = `
-
-
-
-${title}
+    <html>
+        <head>
+            <title>
+            ${title}
+            </title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
