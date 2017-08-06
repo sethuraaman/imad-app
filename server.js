e3c81a5ffd870243fa5ae1ae6c62ffc1626ce661
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-06 18:15:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,100 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-One': {
-        title: 'Article One by Sethuraaman',
-        heading: 'ARTICLE ONE',
-        date: '05 AUG 2017  1700',
-        content:
-        `
-        <p>
-            this is my first web which i was tring it my own
-        </p>
-        `
-    },
-    'article-Two': {
-        title: 'Article Two by Sethuraaman',
-        heading: 'ARTICLE TWO'
-        date: '05 AUG 2017  1800',
-        content:
-        `
-        <p
-            Iam Trying my Best to Do It
-        </p>
-        `
-    },
-    'article-Three': {
-        title: 'Article Three by Sethuraaman',
-        heading: 'ARTICLE THREE',
-        date: '05 AUG 2017;1900',
-        content: 
-        `
-        <p>
-            It Might Be The Correct One
-        </p>
-        `
-    }
-    
-};
 
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-     var htmlTemplate =
-     `
-    <html>
-        <head>
-            <title>
-                <h2>
-                ${title}
-                </h2>
-            </title>
-            <meta name="viewport" content= "width=device-width, intial-scale=1">
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-                <a href="/"><H1>HOME</H1></a>
-            </div>
-            <hr/>
-            <div>
-                <h3>
-                ${heading}
-                </h3>
-            </div>
-            <div>
-                ${date}
-            </div>
-            <div>
-                <h2>
-                ${content}
-                </h2>
-            </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;  
-}
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "ui", "index.html"));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get("/:articleName", function(req,res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get("/ui/style.css", function (req, res) {
-  res.sendFile(path.join(__dirname, "ui", "style.css"));
-});
-
-app.get("/ui/madi.png", function (req, res) {
-  res.sendFile(path.join(__dirname, "ui", "madi.png"));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
