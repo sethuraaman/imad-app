b513366afb753d3cba9c48761ca5b9a2d6e6174c
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-06 19:23:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var articles= {
-    articleone: 'aricle-one.html',
+    'article-one': ('aricle-one.html'),
     articletwo: 'aricle-two.html',
     articlethree: 'aricle-three.html'
 };
