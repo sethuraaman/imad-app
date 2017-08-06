99592e0fd15ac93797b0032c5457a797c62d1655
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-06 22:44:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,109 +6,109 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    articleOne: {
-        title: 'Article One by Sethuraaman',
-        heading: 'ARTICLE ONE',
-        date: '05 AUG 2017;1700',
-        content: `
+
+'postOne': {
+title : 'About Me || GIRIZA',
+heading : 'Post One',
+date : 'Aug 5th,2017',
+content : 
+`    <p>
+        After 4yrs of struggle with coding, i just realised this is the easiest thing ever!
+    </p>
+    <p>
+        Thanks to IMAD! :D
+    </p>`
+},
+'postTwo': {
+title : 'Life as an Engineer || GIRIZA',
+heading : 'Post Two',
+date : 'Aug 6th,2017',
+content : 
+`
+
+
+
+My Life as an Engineer
         <p>
-            this is my first web which i was tring it my own
-        </p>`
-    },
-    articleTwo: {
-        title: 'Article Two by Sethuraaman',
-        heading: 'ARTICLE TWO',
-        date: '05 AUG 2017;1800',
-        content: `
-            <p
-                Iam Trying my Best to Do It
-            </p>`
-    },
-    articleThree: {
-        title: 'Article Three by Sethuraaman',
-        heading: 'ARTICLE THREE',
-        date: '05 AUG 2017;1900',
-        content: `
+            2013-2017
+        </p>
+ `
+},
+'postThree': {
+title : 'Engineer ke Phases || GIRIZA',
+heading : 'Post Three',
+date : 'Aug 7th,2017',
+content : 
+`   <hr/>
+        <h1>
+            Phases of being an Engineer
+        </h1>
+
         <p>
-            It Might Be The Correct One
-        </p>`
-    }
-    
+            My experience as a computer science engineer.
+        </p>
+ `    
 }
+};
 
 function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-     var htmlTemplate =
-     `
-    <html>
-        <head>
-            <title>
-                <h1>
-                ${title}
-                </h1>
-            </title>
-            <meta name='viewport' content= 'width=device-width, intial-scale=1'>
-            <link href='/ui/style.css' rel='stylesheet' />
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = `
+
+
+
+${title}
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
-            <div class='container'>
-                <a href='/'><H1>HOME</H1></a>
-            </div>
-            <hr/>
-            <div>
-                <h3>
-                ${heading}
-                </h3>
-            </div>
-            <div>
-                ${date}
-            </div>
-            <div>
-                <h2>
-                ${content}
-                </h2>
+            <div class="container">
+                    <div>
+                         <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h1>
+                        ${heading}
+                    </h1>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                       ${content}
+                    </div>
             </div>
         </body>
     </html>
-    `;
-    return htmlTemplate  
+
+`;
+return htmlTemplate;
 }
-    
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName= req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/article-two', function (req, res){
-    res.sendFile(path.join(__dirname,'ui', 'article-two.html'));
-});
-
-app.get('/article-Three', function (req, res){
-    res.send('article Three will be served')
+app.get('/:postName', function(req,res) {
+var postName = req.params.postName;
+res.send(createTemplate(articles[postName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
-// Do not change port, otherwise your app won"t run on IMAD servers
+// Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(port, function ()) {
+console.log(`IMAD course app listening on port ${port}!`);
 });
+
