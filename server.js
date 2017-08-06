f172d71e5b9369b3073b4af4de57652adec1b919
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-06 18:32:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(__dirname, 'ui', 'Article-one.html')
+    res.sendFile((path.join__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/article-two', function (req, res){
