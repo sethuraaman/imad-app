6f069f704dd9531a394cbb1c0084fdb28083ff30
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-07 08:46:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,22 +5,31 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/articleOne', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'Article-One.html'));
-});
-
-app.get('/artilceTwo', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/articleThree', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article one', function(req,res) {
+    //var postName = req.params.postName;
+    res.sendFile(path.join(__dirname,'ui', 'Article-one.html'));
+});
+
+app.get('/Article two', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/Article three', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
