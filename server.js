d5d33b53ee9824b8a294af4bd22604d643d44b8f
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-08 23:43:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,11 +79,11 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-three', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+//app.get('/Article-three', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+//});
 
-//app.get('/:articleName', function(req, res) {
+app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
