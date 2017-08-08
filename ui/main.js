a5d14a4fab435c439be4e8ca3f5b861cbe0d5163
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-09 01:38:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ var element = document.getElementById("main text");
 element.innerHTML = 'M A N I';
 
 var img = document.getElementBId('madi');
-   img.onclick = function(){
+   img.onclick = function () {
        img.style.marginleft = '50px';
    };
