37ad351e9a637ce4936e954cc64a3dcffe733189
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-10 11:32:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,10 @@ app.get('/ui/style.css', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
 app.get('/ui/main.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
