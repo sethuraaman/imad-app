d7222471dfd8540447fa45630ee18762011dd0f9
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-13 02:17:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,8 @@ submit.onClick = function() {
     
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for (var i = 0; i<name.length; i++){
-        list += '<li>'+name[i]+'<li>';
+    for (var i=0; i< names.length; i++){
+        list += '<li>'+names[i]+'<li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML =list;
