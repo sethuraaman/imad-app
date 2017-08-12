8b9a9ea7b27c7fcc5c3c611f41b9d3421cc6fdfa
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-13 01:00:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 var button = document.getElementById('counter');
 
-counter = 0;
-
-button.onclick = function (){
-     counter = counter +1;
-     var span = document.getElementById('count');
-     span.innerHTML=counter.toString();
+button.onclictk = function (){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.Done){
+            if request status === 200;){
+                var counter = document.getElementById('counter');
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+    };
+    request.open('GET', 'http://sethu18rr.imad.hasura-app.io/counter', true);
+    request.send(null);
  };
