c2f278545ca5a693afac2bcfa6f38f6b90dba7e8
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-12 22:44:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ button.onclick = function(){
             }
         }
     };
-    request.open('GET', 'http://sethu18rr.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://sethu18rr.imad.hasura-app.io/counter', true);
     request.send(null);
  };
