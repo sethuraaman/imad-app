d10a752ed3c16b302684ffed2d9cab188b003f39
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-12 20:48:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,7 @@
-console.log('Loaded!');
-
-var element = document.getElementById("main text");
-
-element.innerHTML = 'M A N I';
-
-var img = document.getElementById('madi');
-var marginLeft = 0 ;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft=marginLeft+'px';
-}
-   img.onclick = function () {
-       var interval = setInterval(moveRight, 100);
-   };
+var button = document.getElementById('counter');
+counter = 0;
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
