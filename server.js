0bdbf6a4d5142d36c051071439b0d31487c0633e
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-17 07:50:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/counter',function(req,res){
 
 var names = [];
 app.get('/submit-name/:name', function(req,res){
-    var name = req.query.name;
+    var name = req.params.name;
     names.push(name);
     res.send(JSON.stringyfy(names));
 });
