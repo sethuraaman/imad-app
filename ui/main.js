9547786161ce712aa78c55879d9644a02f300fa4
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-17 07:41:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,27 +14,15 @@ function exec(){
     request.send(); 
     console.log('finished');
 }
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onClick= function(){
-    console.log('EXECUTED');
-var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-          if (request.readyState === XMLHttpRequest.DONE){
-             if (request.status === 200) { 
-                    var names = request.responseText; 
-                    names=JSON.parse(names);
-                    var list = '';
-                    for (var i =0; i < names.length; i++) {
-                        list += '<li>' +name[i] + '</li>';
-                    }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-                } 
-          } 
-    }; 
-    var nameInput=document.getElementById('name');
-    var name = nameInput.value;
-    request.open("GET", "http://sethu18rr.imad.hasura-app.io/submit-name?name=" + name, true);
-    console.code('executed');
-    request.send(null); 
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i =0; i < names.length; i++) {
+    list += '<li>' +name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
