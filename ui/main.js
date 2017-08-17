fd63d11bcda3b266d12e19dd2699547766dafde8
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-17 08:33:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,16 +14,20 @@ function exec(){
     request.send(); 
     console.log('finished');
 }
-var nameInput=document.getElementById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onClick= function(){
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i =0; i < names.length; i++) {
-    list += '<li>' +name[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+submit.onclick = function () {
+//make a request to the server
+
+//capture a list of names and render it as a list
+var names = ['name1','name2','name3'];
+var list = '';
+for(var i=0;i< names.length; i++)
+{
+    list +='<li>' + names[i] + '</li>';
+}
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
 };
     console.log ('executing');
