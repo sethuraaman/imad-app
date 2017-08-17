fc3dafd086c925b7c869edb52d989a3bee5cb511
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-17 08:05:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,7 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onClick= function(){
+    console.log ('executing')
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i =0; i < names.length; i++) {
