c05492fa3498fc9a389dc0efb37a85d94ec6b6c0
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-17 09:26:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,7 @@ function exec(){
     request.send(); 
     console.log('finished');
 }
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     var request = new XMLHttpRequest();
@@ -35,6 +34,8 @@ submit.onclick = function () {
                 } 
           } 
     }; 
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open("GET", "http://sethu18rr.imad.hasura-app.io/submit-name?name="+ name, true);
     request.send(); 
     //make a request to the server
