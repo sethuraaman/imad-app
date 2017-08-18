8d976b5bd185176e6a97ec709ec6297dde2cd2eb
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-18 14:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ var names = [];
 app.get('/submit-name', function(req,res){
     var name = req.query.name;
     names.push(name);
-    res.send(JSON.stringify(name));
+    res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function(req, res) {
