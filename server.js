39d4bc342f7121f63fb189a2e3279a84c93b12b5
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-21 22:21:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/articles/:articleName', function(req, res) {
        }
        else {
            var articleData = result.rows[0];
-           res.send(createTamplate(articleData));
+           res.send(createTemplate(articleData));
        } 
     });
 });
