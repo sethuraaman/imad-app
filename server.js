d11926b5b6673183755021cb86d75eede4e2584f
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-21 22:36:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function createTemplate (data) {
                 <hr/>
                 <h3> ${heading}</h3>
                 <DIV>
-                    ${date.toDataString()}
+                    ${date}
                 </DIV>
                 <DIV>
                     ${content}
