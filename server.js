d4695a452bffc80ec930edef7762db10d65cbbe3
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-08-24 12:18:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require ('crypto');
+var bodyParser = require.('body-parser')
 
 
 var config = {
@@ -15,6 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
    var title = data.title;
@@ -66,6 +68,43 @@ app.get('/hash/:input',function (req, res){
     res.send(hashedString);
 });
 
+app.post('create-user', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   vardbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username, dbString], function(err,result){
+       if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: '+ username);
+        }
+   });
+});
+
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username =$1', [username], function(err,result){
+       if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length ===0) {
+                res.send(403).send('username/passwordis invalid');
+            } else{
+                var dbString = result.rows[0].password;
+                var salt = dbstring.split('$')[2];
+                var hashedPassword = hash(password, salt);
+                if(hashedPassword === dbString) {
+                    res.send('credentials are correct!');
+                } else {
+                    res.send(403).send('username/password is invalid');
+                }
+            }
+        }
+   });
+});
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     pool.query('SELECT * FROM test', function (err, result){
