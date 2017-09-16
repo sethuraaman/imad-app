84d2d43c03538d68649f9c10d9baebbbf91d78bd
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-09-16 09:35:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,9 +22,9 @@ submit.onclick = function (){
    request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE){
          if (request.status === 200 ){
-	    var names = request.reponseText;
+	    var names = request.responseText;
 	    names = JSON.parse(names);
-	    var liSt = '';
+	    var list = '';
 	    for (var i=0; i< names.length; i++){
 	       list += '<li>' + names[i]+'</li>';
 	    }
