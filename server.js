d11a6a941c799ff19bd22f102b321685c91533b0
sethuraaman <30649453+sethuraaman@users.noreply.github.com>
2017-10-07 12:38:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,65 +1,24 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').Pool;
+var http = require('http');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'sethu18rr',
     database: 'sethu18rr',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: 'db-sethu18rr-22215'
 };
 var app = express();
 app.use(morgan('combined'));
-
-var articles = {
-    articleone: {
-	herf:'Home',
-	link1:'<a href="/articletwo">Article-two</a>',
-	link2:'<a href="/articlethree">Article-three</a>',
-       title: 'Article One || Sethuraaman',
-       date: '05 aug 2017',
-       heading: 'Article One',
-       content: `
-	   <p>
-	   <h1> This is my own first app designing learnt from IMAD </h1>
-	   </p>`
-    },
-    
-    articletwo: {
-	herf:'Home',
-        link1:'<a href="/articleone">Article-one</a>',
-	link2:'<a href="/articlethree">Article-three</a>',
-       	title: 'Aritcle Two || Sethuraaman',
-        date: '06 Aug 2017',
-        heading: 'Article Two',
-        content: `
-          <p>
-             <h1> this is my trial to reduce the coding </h1> 
-            </p> `
-    },
-    articlethree: {
-	herf:'Home',
-        link1:'<a href="/articleone">Article-one</a>',
-	link2:'<a href="/articletwo">Article-two</a>',
-       title:'Article Three || Sethuraaman',
-        date:'07 Aug 2017',
-        heading:'Article Three',
-        content:`
-        <p>
-        <h1> this is the confirmed page which i created with the help of 
-
-IMAD </h1>
-        </p>`
-    }
-};
 
 function createTemplate (data) {
    var title = data.title;
    var date = data.date;
    var heading = data.heading;
    var content = data.content;
-   var herf = data.herf;
    var link1 = data.link1;
    var link2 = data.link2;
    
@@ -70,22 +29,20 @@ function createTemplate (data) {
             <title>
                 ${title}
             </title>
-            <meta name="viewport" content="width=device-width, initial-
-
-scale=1">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="/ui/style.css" rel="stylesheet">
         </head>
         <body>
             <div class="container">
                 <div>
-                    <a href="/">${herf}</a>
+                    <a href="/">Home</a>
                     ${link1}
                     ${link2}
                 </div>
                 <hr/>
                 <h3> ${heading}</h3>
                 <DIV>
-                    ${date}
+                    ${date.toDateString()}
                 </DIV>
                 <DIV>
                     ${content}
@@ -101,13 +58,35 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input, salt){
+   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+   return ["pbkdf2", "10000",salt ,hashed.toString('hex')];
+}
+
+app.get('/hash/:input', function(req, res){
+   var hashedString = hash(req.params.input, 'this-is-some-randam-string');
+   res.send(hashedString);
+});
+
+app.get('/create-user', function (req, res){
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   vardbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES($1, $2)', [username, dbString], function (err, result){
+      if(err){
+	     res.status(500).send(err.toString());
+      } else {
+	     res.send('user successfully created: ' + username);
+      }
+   });
+});
+
 var pool = new Pool(config);
-app.get('/test-db', function(req, res) {
+app.get('/test-db', function(req, res){
    pool.query('SELECT * FROM test', function (err, result){
       if(err){
-	 res.status(500).send(err.toString());
+	     res.status(500).send(err.toString());
       } else {
-	 res.send(JSON.Stringify(result));
+	     res.send(JSON.stringify(result));
       }
    });
 });
@@ -126,8 +105,18 @@ app.get('/submit-name',function(req,res){
 });
 
 app.get('/:articleName', function(req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result){
+        if (err){
+	        res.status(500).send(err.toString());
+	    } else {
+	        if (result.rows.length === 0) {
+		        res.status(404).send('Article not found');
+	        } else{
+   	            var articleData = result.rows[0];
+	        	res.send(createTemplate(articleData));
+	        }
+	    }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
